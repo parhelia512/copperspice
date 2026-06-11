318edf902ccc7c424677dcd72d2c303b2f249cbd
Barbara Geller <barbara@copperspice.com>
2026-06-11 15:20:56 -0700

    Move the lowest supported version from Windows 7 to Windows 10

--- src/core/global/qt_windows.h
+++ src/core/global/qt_windows.h
@@ -24,90 +24,12 @@
 #ifndef QT_WINDOWS_H
 #define QT_WINDOWS_H
 
-#if defined(Q_CC_MINGW)
-
-// mingw windows.h file does not set this value properly, only place in CopperSpice WINVER is defined
-// this define must come before including windows.h
+// following define must come before including windows.h
 
 #ifndef WINVER
-#define WINVER 0x601         // Windows 7
-#endif
-
+#define WINVER 0xA00         // Windows 10
 #endif
 
 #include <windows.h>
-
-// already defined when compiled with WINVER >= 0x0500
-#ifndef SPI_SETMENUANIMATION
-#define SPI_SETMENUANIMATION 0x1003
-#endif
-#ifndef SPI_SETMENUFADE
-#define SPI_SETMENUFADE 0x1013
-#endif
-#ifndef SPI_SETCOMBOBOXANIMATION
-#define SPI_SETCOMBOBOXANIMATION 0x1005
-#endif
-#ifndef SPI_SETTOOLTIPANIMATION
-#define SPI_SETTOOLTIPANIMATION 0x1017
-#endif
-#ifndef SPI_SETTOOLTIPFADE
-#define SPI_SETTOOLTIPFADE 0x1019
-#endif
-#ifndef SPI_SETUIEFFECTS
-#define SPI_SETUIEFFECTS 0x103F
-#endif
-#ifndef SPI_GETMENUANIMATION
-#define SPI_GETMENUANIMATION 0x1002
-#endif
-#ifndef SPI_GETMENUFADE
-#define SPI_GETMENUFADE 0x1012
-#endif
-#ifndef SPI_GETCOMBOBOXANIMATION
-#define SPI_GETCOMBOBOXANIMATION 0x1004
-#endif
-#ifndef SPI_GETTOOLTIPANIMATION
-#define SPI_GETTOOLTIPANIMATION 0x1016
-#endif
-#ifndef SPI_GETTOOLTIPFADE
-#define SPI_GETTOOLTIPFADE 0x1018
-#endif
-#ifndef SPI_GETUIEFFECTS
-#define SPI_GETUIEFFECTS 0x103E
-#endif
-#ifndef SPI_GETKEYBOARDCUES
-#define SPI_GETKEYBOARDCUES 0x100A
-#endif
-#ifndef SPI_GETGRADIENTCAPTIONS
-#define SPI_GETGRADIENTCAPTIONS 0x1008
-#endif
-#ifndef IDC_HAND
-#define IDC_HAND MAKEINTRESOURCE(32649)
-#endif
-#ifndef WM_MOUSEWHEEL
-#define WM_MOUSEWHEEL 0x020A
-#endif
-#ifndef WM_MOUSEHWHEEL
-#define WM_MOUSEHWHEEL 0x020E
-#endif
-#ifndef ETO_PDY
-#define ETO_PDY 0x2000
-#endif
-#ifndef COLOR_GRADIENTACTIVECAPTION
-#define COLOR_GRADIENTACTIVECAPTION 27
-#endif
-#ifndef COLOR_GRADIENTINACTIVECAPTION
-#define COLOR_GRADIENTINACTIVECAPTION 28
-#endif
-
-// already defined when compiled with WINVER >= 0x0600
-#ifndef SPI_GETFLATMENU
-#define SPI_GETFLATMENU 0x1022
-#endif
-#ifndef CS_DROPSHADOW
-#define CS_DROPSHADOW 0x00020000
-#endif
-#ifndef CLEARTYPE_QUALITY
-#define CLEARTYPE_QUALITY 5
-#endif
 
 #endif
